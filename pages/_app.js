2ed7fee577f7518e9c2778f7bdc9b10c32904fd0
gaoey <gaoey@hotmail.com>
2021-01-29 17:20:01 +0700

    remove unused code

--- pages/_app.js
+++ pages/_app.js
@@ -4,7 +4,7 @@ import * as gtag from '../lib/gtag';
 
 
 // Notice how we track pageview when route is changed
-Router.events.on('routeChangeComplete', (url) => console.log({ url }) || gtag.pageview(url))
+Router.events.on('routeChangeComplete', (url) => gtag.pageview(url))
 
 function Application({ Component, pageProps }) {
   return <Component {...pageProps} />
