d98db2fb9959c93d86160188c094b4bcae136618
gaoey <gaoey@hotmail.com>
2021-01-29 16:26:11 +0700

    add ga to test

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,15 @@
 import '@styles/globals.css'
+import { useEffect } from 'react'
+import ReactGA from 'react-ga';
 
 function Application({ Component, pageProps }) {
+  useEffect(() => {
+    if (typeof window !== 'undefined') {
+      ReactGA.initialize('UA-188191546-1');
+      ReactGA.pageview(window.location.pathname + window.location.search);
+    }
+  })
+
   return <Component {...pageProps} />
 }
 
